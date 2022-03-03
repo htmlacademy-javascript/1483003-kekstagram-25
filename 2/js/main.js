e43b976583af579be6cffd07834bcde9f40d9fe9
Keks <keks@htmlacademy.ru>
2022-03-03 17:56:25 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,28 +1,44 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-// Функция, возвращающая случайное целое число из переданного диапазона включительно
+function isNumber(val) {
+  return typeof val === 'number';
+}
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min); // Округление вверх до ближайшего большего целого
-  max = Math.floor(max); // Округление вниз до ближайшего меньшего целого
-  if (min < 0 || max < 0) {
-    return 0;
+function isPositiveMaxNumbers() {
+  return Math.max(...arguments) > 0;
+}
+
+// Функция возвращающая случайное целое число из переданного диапазона включительно
+
+function getRandomIntInclusive(from, to) {
+  if (!isNumber(from) || !isNumber(to) || !isPositiveMaxNumbers(from, to)) {
+    return null;
   }
-  if (min === max) {
-    return min;
+  if (from < 0) {
+    from = 0;
   }
-  if (min > max) {
-    [min, max] = [max, min];
+  if (to < 0) {
+    to = 0;
+  }
+  if (from > to) {
+    [from, to] = [to, from];
+  }
+  const min = Math.ceil(from); // Округление вверх до ближайшего большего целого
+  const max = Math.floor(to); // Округление вниз до ближайшего меньшего целого
+  if (from === to) {
+    return from;
   }
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
 getRandomIntInclusive(100, 50);
 
-// Функция для проверки максимальной длины строки
+/**
+ * @description Функция проверяет, что строка меньше или равна заданной длине
+ * @param {string} string - проверяемая строка
+ * @param {number} maxLength - максимальная длина
+ * @return {boolean}
+ */
+const checkStringMaxLength = (string, maxLength) => string.length <= maxLength;
 
-function checkStringLength(checkedString, maxLength) {
-  return checkedString.length <= maxLength;
-}
-
-checkStringLength('Привет', 5);
+checkStringMaxLength('Привет', 10);
