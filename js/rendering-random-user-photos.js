229fb6abdff9c7dabf8103c8e3ee2c1b73c55aa8
Eremeev Alexander <sanya.eremey@mail.ru>
2022-03-16 09:38:47 +0300

    Генерация фотографий других пользователей

---
+++ js/rendering-random-user-photos.js
@@ -0,0 +1,18 @@
+import {makeUserPosts} from './generating-posts.js';
+
+const galleryRandomUsersPhotos = document.querySelector('.pictures'); // Секция для вставки отрисованных фотографий случайных пользователей
+const userPhotoTemplate = document.querySelector('#picture').content.querySelector('.picture'); // Шаблон для заполнения данными фотографии случайного пользователя
+
+const userPosts = makeUserPosts();
+
+const photosGalleryFragment = document.createDocumentFragment();
+
+userPosts.forEach((userPost) => {
+  const userPhoto = userPhotoTemplate.cloneNode(true);
+  userPhoto.querySelector('.picture__img').src = userPost.url;
+  userPhoto.querySelector('.picture__comments').textContent = userPost.comments.length;
+  userPhoto.querySelector('.picture__likes').textContent = userPost.likes;
+  photosGalleryFragment.appendChild(userPhoto);
+});
+
+galleryRandomUsersPhotos.appendChild(photosGalleryFragment);
